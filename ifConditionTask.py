964476ed40217702ac4048040c5e61f0d0bd6ebf
vidhya1506 <vidhyavidhu1506@gmail.com>
2024-02-14 18:32:44 +0530

    14-feb-2024

--- ifConditionTask.py
+++ ifConditionTask.py
@@ -1,4 +1,27 @@
-"""#3.check given number is +ve orr -ve
+"""1.find minimum or maximum
+
+value1= int(input("enter First Value"))
+value2= int(input("enter second Value"))
+if(value1> value2):
+    print("Value 1 is Greater than Value 2")
+elif(value1<value2):
+    print("Value 2 is Greater than Value 1")
+elif(value1 == value2):
+    print("Both Value1 and Value2 are Equal")
+2.find maximum or minimum in three value
+
+val1= int(input("enter First Value"))
+val2= int(input("enter second Value"))
+val3= int(input("enter Third Value"))
+if(val1> val2  and val1>val3):
+    print("Value 1 is Greater than Value 2")
+elif(val3<val2 and val2>val1):
+    print("Value 2 is Greater than Value 1")
+else:
+    print(" Value3 is greater")
+
+
+#3.check given number is +ve orr -ve
 a =  float (input("Enter the Number"))
 if a>0:
     print("The Given Value is +ve")
@@ -173,3 +196,107 @@ elif(triSide1 == triSide2 or triSide2 == triSide3 or triSide1 == triSide3):
     print("This triangle is isosceles ")
 else:
     print("This is Scalene Triangle")"""
+"""16.check profit or loss
+
+costPrice = int(input("Enter The Cost Price"))
+sellingPrice = int (input("Enter The Selling Price"))
+amount = 0
+
+if(sellingPrice > costPrice):
+    amount = sellingPrice-costPrice
+    print(f"Profit Value is {amount}")
+elif(sellingPrice < costPrice):
+    amount = costPrice - sellingPrice
+    print(f"Loss Value is {amount}")
+else:
+    print("No profit and Loss")"""
+
+
+"""17.enter student marks and find percentage and grade
+
+tamilMark = int(input("Enter the Tamil Mark"))
+englishMark = int(input("Enter the English Mark"))
+mathsMark = int(input("Enter the Maths Mark"))
+sciMark = int(input("Enter the Science Mark"))
+socMark = int(input("Enter the Social Mark"))
+total = tamilMark+englishMark+mathsMark+sciMark+socMark
+print(f"The Total Mark is {total}")
+percentage = int (total/5.0)
+print(f"The Percentage is {percentage}")
+if(percentage>=90):
+    print("Grade A")
+elif(percentage>=80):
+    print("Grade B")
+elif(percentage>=70):
+    print("Grade C")
+elif(percentage>=60):
+    print("Grade D")
+elif(percentage>=40):
+    print("Grade E")
+else:
+    print("Grade F")
+"""    
+"""18.calculate gross salary of Employee
+
+basicSalary = int(input("Enter Employee Basic Salary"))
+
+if(basicSalary<=10000):
+    dailyAllowence = basicSalary+0.8
+    hra= basicSalary*0.2
+elif(basicSalary<=20000):
+    dailyAllowence = basicSalary+0.9
+    hra= basicSalary*0.25
+else:
+    dailyAllowence = basicSalary+9.5
+    hra= basicSalary*0.3
+grossSalary = basicSalary+dailyAllowence+hra
+print(f"The Gross Salary of Employee is {grossSalary}")
+
+"""
+"""19
+
+a= int(input("Enter the Number:")
+b= int(input("Enter the Number:")
+c= int(input("Enter the Number:")
+val=(b*b)-(4*a*c)
+
+if(val>0):
+    root1=(-b+sqrt(val))/(2*a)
+    root2=(-b+sqrt(val))/(2*a)
+    print("Root1:",root1)
+    print("Root2:",root2)
+elif(val==0)
+    root1= root2=-b/(2*a)
+    print("Root1:",root1)
+    print("Root2:",root2)
+else:
+    root1=root2=-b/(2*a)
+    i=sqrt(-val)/(2*a)
+    print("Root1:",root1)
+    print("Root2:",root2)
+    print("Imaginary:",i)
+
+20
+unit=int(input("Enter the Units:")
+billAmt=0
+if(unit<=50):
+    billAmt=unit*0.5
+elif(unit<=150):
+    billAmt=25+((unit-50)*0.75)
+elif(unit<=250):
+    billAmt=62.5+((unit-150)*1.20)
+else:
+    billAmt=220+((unit-250)*1.50)
+tax=bill*0.2
+total=billAmt+tax
+print("The Total Bill is {total}")
+"""    
+"21.Check given text is character or digit or spl character"
+
+character=input("Enter any Character")
+if(character.isalpha()):
+    print("The Entered character is an alphabet")
+elif(character.isdigit()):
+    print("The Entered character is Digit")
+else:
+    print("The Entered character is spl Character")
